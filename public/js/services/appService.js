267232419f1cda8be13d8f5e84fb12fc137dc97b
Jonathan <jonathannael12@gmail.com>
2017-03-13 08:47:09 +0000

    change number format

--- public/js/services/appService.js
+++ public/js/services/appService.js
@@ -4,6 +4,7 @@ angular.module('appService', [])
 		return {
 			getWeatherByCity : function(name) {
 				return $http.get('http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=5&APPID=481e3bc28e5264e5607c2b65b449bfc1&q='+name)
+								 // api.openweathermap.org/data/2.5/forecast
 			}
 		}
 	}]);
